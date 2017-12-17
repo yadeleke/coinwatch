a499aacb526dcd5eaadab54594fe9131ff9a12e4
Adeyinka Adeleke <yinka@Adeyinkas-MacBook-Pro.local>
2017-12-17 09:52:45 +0000

    added refreshControl

--- app/screens/Feed.js
+++ app/screens/Feed.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  ScrollView
-} from 'react-native';
+import { Text,View, ScrollView, RefreshControl } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 import Numeral from "numeral";
 
@@ -16,10 +12,12 @@ class Feed extends Component {
       selectedCoin: null,
       Bitcoin: null,
       Ethereum: null,
-      Litecoin: null
+      Litecoin: null,
+      refreshing: false
     }
     this.fetchData = this.fetchData.bind(this);
   }
+
   onLearnMore = (coin) => {
     this.setState({
       selectedCoin: coin.CoinName
@@ -28,35 +26,54 @@ class Feed extends Component {
   };
 
   fetchData(){
+    this.setState({refreshing: true});
     let price = '';
     const url1 = 'https://api.coinmarketcap.com/v1/ticker/bitcoin' 
     const url2 = 'https://api.coinmarketcap.com/v1/ticker/ethereum' 
     const url3 = 'https://api.coinmarketcap.com/v1/ticker/litecoin' 
-    fetch(url1).then(response => response.json()
-              .then(responseJson => {
-                this.setState({
-                  Bitcoin: Numeral(responseJson[0].price_usd).format('$0,0.00')
-                 })
-              }).catch((err) => {
-        console.log(err);
-    }))
-    fetch(url2).then(response => response.json()
-              .then(responseJson => {
-                this.setState({
-                  Ethereum: Numeral(responseJson[0].price_usd).format('$0,0.00')
-                 })
-              }).catch((err) => {
-        console.log(err);
-    }))
-    fetch(url3).then(response => response.json()
-              .then(responseJson => {
-                this.setState({
-                  Litecoin: Numeral(responseJson[0].price_usd).format('$0,0.00')
-                 }) 
-              }).catch((err) => {
-        console.log(err);
-    }))
-  }
+
+    Promise.all([
+      fetch("https://api.coinmarketcap.com/v1/ticker/bitcoin"),
+      fetch("https://api.coinmarketcap.com/v1/ticker/ethereum"),
+      fetch("https://api.coinmarketcap.com/v1/ticker/litecoin"),
+    ]).then((responseArray) => Promise.all(responseArray.map(item => item.json()))).then((responseJsonArray) => {
+        console.log(responseJsonArray);
+        this.setState({
+          Bitcoin: Numeral(responseJsonArray[0][0].price_usd).format('$0,0.00'),
+          Ethereum: Numeral(responseJsonArray[1][0].price_usd).format('$0,0.00'),
+          Litecoin: Numeral(responseJsonArray[2][0].price_usd).format('$0,0.00'),
+          refreshing: false
+        })
+      });
+    }
+  
+     
+    // fetch(url1).then(response => response.json()
+    //           .then(responseJson => {
+    //             this.setState({
+    //               Bitcoin: Numeral(responseJson[0].price_usd).format('$0,0.00')
+    //              })
+    //           }).catch((err) => {
+    //     console.log(err);
+    // })).then(
+    // fetch(url2).then(response => response.json()
+    //           .then(responseJson => {
+    //             this.setState({
+    //               Ethereum: Numeral(responseJson[0].price_usd).format('$0,0.00')
+    //              })
+    //           }).catch((err) => {
+    //     console.log(err);
+    // }))).then(
+    // fetch(url3).then(response => response.json()
+    //           .then(responseJson => {
+    //             this.setState({
+    //               Litecoin: Numeral(responseJson[0].price_usd).format('$0,0.00')
+    //              }) 
+    //           }).catch((err) => {
+    //     console.log(err);
+    // }))).then(() => {
+    //   this.setState({refreshing: false});
+    // });
 
   componentDidMount() {
     this.fetchData();
@@ -64,7 +81,13 @@ class Feed extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this.fetchData}
+          />
+        }>
         <List>
           {coins.map((coin) => (
             <ListItem
